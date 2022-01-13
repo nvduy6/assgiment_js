d8b480daea875e3ea6dd7f9584c7813cde071dcc
duynvph15542 <duynvph15542@fpt.edu.vn>
2022-01-13 10:44:33 +0700

    Tạo page và ui admin

--- src/comporents/header.js
+++ src/comporents/header.js
@@ -11,18 +11,24 @@ const Header = {
     <div class="h-12 bg-[#cb7801]">
         <nav class="flex justify-between">
             <ul class="leading-10" id="menu">
-                <li class="inline font-medium py-3 px-4 text-white  hover:text-cyan-900"><a href="/">Trang chủ</a></li>
-                <li class="inline font-medium py-3 px-4 text-white  hover:text-cyan-900"><a href="/about">Tuyển sinh</a></li>
-                <li class="inline font-medium py-3 px-4 text-white  hover:text-cyan-900"><a href="">Chương trình đào tạo</a></li>
-                <li class="inline font-medium py-3 px-4 text-white  hover:text-cyan-900"><a href="">Góc sinh viên</a></li>
-                <li class="inline font-medium py-3 px-4 text-white  hover:text-cyan-900"><a href="">Tuyển dụng</a></li>
+                <li class="inline font-medium py-1 px-1 text-white  hover:text-cyan-900"><a href="/">Trang chủ</a></li>
+                <li class="inline font-medium py-1 px-1 text-white  hover:text-cyan-900"><a href="/about">Tuyển sinh</a></li>
+                <li class="inline font-medium py-1 px-1 text-white  hover:text-cyan-900"><a href="/about">Chương trình đào tạo</a></li>
+                <li class="inline font-medium py-1 px-1 text-white  hover:text-cyan-900"><a href="/about">Góc sinh viên</a></li>
+                <li class="inline font-medium py-1 px-1 text-white  hover:text-cyan-900"><a href="">Tuyển dụng</a></li>
             </ul>
-            <div class=" mr-0.5 mt-2.5 mr-4">
+            <div class=" mt-2.5 ">
                 <input class="search_item" type="text">
                 <input class="bg-cyan-900 text-white w-20 font-medium" type="button" value="Tìm kiếm">
             </div>
+            <div class="mt-2">
+                 <li class="inline font-medium ml-2 mt-2  text-white  hover:text-cyan-900"><a href="/sign">Đăng ký</a></li>
+                <li class="inline font-medium ml-1  text-white  hover:text-cyan-900"><a href="/login">Đăng nhập</a></li>
+                <li class="inline font-medium ml-1 text-white  hover:text-cyan-900"><a href="/admin/dashboard">Admin</a></li>
+            </div>
         </nav>
     </div>
+
         `
     }
 }
--- src/main.js
+++ src/main.js
@@ -5,12 +5,25 @@ import Header from "./comporents/header";
 import Footer from "./comporents/footer";
 import NewsPage from "./pages/news";
 import DetailNewsPage from "./pages/detailNews";
-const router = new Navigo("/", { linksSelector: "a" });
+import Login from "./pages/login";
+import Sign from "./pages/sign";
+import Dashboard from "./comporents/dashboard";
+import HomeDashboard from "./admin/home";
+// import NewList from "./comporents/nexlist";
+import AddNews from "./admin/addnew";
+import EditNews from "./admin/editnew";
+import NewListstudent from "./admin/new";
+const router = new Navigo("/");
 const print = (content) => {
     document.getElementById("header").innerHTML = Header.render();
     document.getElementById("app").innerHTML = content;
     document.getElementById("footer").innerHTML = Footer.render();
 
+};
+
+const AdminPrint = (page, id = '') => {
+    const layout = `${Dashboard.render(id)} ${page.render()}`;
+    document.getElementById("root").innerHTML = layout;
 };
 router.on({
     "/": () => {
@@ -19,13 +32,36 @@ router.on({
     "/about": () => {
         print(AboutPage.render());
     },
+    "/login": () => {
+        print(Login.render());
+    },
+    "/sign": () => {
+        print(Sign.render());
+    },
     "/news": () => {
         print(NewsPage.render());
     },
     "/news/:id": ({ data }) => {
         const { id } = data;
         print(DetailNewsPage.render(id));
+    },
+    "/admin/dashboard": () => {
+        AdminPrint(HomeDashboard);
+    },
+    "/admin/news": () => {
+        AdminPrint(NewListstudent);
+    },
+    "/admin/news/add": () => {
+        AdminPrint(AddNews);
+    },
+    "/admin/news/:id/edit": ({ data }) => {
+        const { id } = data;
+        AdminPrint(EditNews.render(id));
     }
 
+});
+router.notFound((match) => {
+
+    console.log("Not found", match);
 });
 router.resolve();
